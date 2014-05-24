170b7875aeb614e8c1510f5c805ab6a8a005cf5b
Alexey Chumakov <achumakov@exadel.com>
2014-05-24 16:00:12 +0300

    Initial project

---
+++ c-mapper/src/main/java/com/exadel/c-mapper/App.java
@@ -0,0 +1,13 @@
+package com.exadel.c-mapper;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
